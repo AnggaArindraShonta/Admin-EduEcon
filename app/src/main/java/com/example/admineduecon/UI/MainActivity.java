566460e725e214ddcd0810aa75b9205e131745b4
AnggaArindraShonta <angga.shonta@students.amikom.ac.id>
2021-07-16 20:56:29 +0700

    Admin EduEcon

---
+++ app/src/main/java/com/example/admineduecon/UI/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.admineduecon.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.admineduecon.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView addEbook, addVideo, ebook;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addEbook = findViewById(R.id.addEbook);
+        addVideo = findViewById(R.id.addVideo);
+        ebook = findViewById(R.id.ebook);
+
+        addEbook.setOnClickListener(this);
+        addVideo.setOnClickListener(this);
+        ebook.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.addEbook:
+                intent = new Intent(MainActivity.this, UploadEbookActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.addVideo:
+                intent = new Intent(MainActivity.this, VideoViewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.ebook:
+                intent = new Intent(MainActivity.this, EbookActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/admineduecon/UI/VideoViewActivity.java
@@ -0,0 +1,83 @@
+package com.example.admineduecon.UI;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.admineduecon.Adapter.VideoAdapter;
+import com.example.admineduecon.Model.Video;
+import com.example.admineduecon.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class VideoViewActivity extends AppCompatActivity {
+
+    //UI views
+    FloatingActionButton addVideoBtn;
+    private RecyclerView rvVideo;
+    private ArrayList<Video> videoArrayList;
+    private VideoAdapter adapterVideo;
+    private static int REQUEST_CODE=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, REQUEST_CODE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+
+        setTitle("Videos");
+
+        addVideoBtn = findViewById(R.id.addVideoBtn);
+        rvVideo = findViewById(R.id.rvVideo);
+
+
+        loadVideoFromFirebase();
+
+        addVideoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VideoViewActivity.this, UploadVideoActivity.class));
+            }
+        });
+    }
+
+    private void loadVideoFromFirebase() {
+        videoArrayList = new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Videos");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                videoArrayList.clear();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Video video = dataSnapshot.getValue(Video.class);
+                    videoArrayList.add(video);
+                }
+                adapterVideo = new VideoAdapter(VideoViewActivity.this, videoArrayList);
+                rvVideo.setAdapter(adapterVideo);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
